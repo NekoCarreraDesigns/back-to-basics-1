c9f217ff475a39833a4549eb634a4c3f39d90d6b
NekoCarreraDesigns <denvermaas@gmail.com>
2022-05-02 12:50:31 -0600

    added script for attribute manipulation

--- index.js
+++ index.js
@@ -47,3 +47,57 @@ mainDiv.childNodes[1].style.textDecoration = "underline";
 mainDiv.lastElementChild.style.fontSize = "50px";
 mainDiv.firstElementChild.style.color = "orange";
 mainDiv.lastElementChild.parentElement.style.fontSize = "40px";
+
+let main = document.querySelector("main-div");
+let sitesElement = document.querySelector(".sites");
+let site1Element = document.querySelector(".site1");
+let site2Element = document.querySelector(".site2");
+let site3Element = document.querySelector(".site3");
+let siteTitles = document.querySelector("h4");
+
+let sites = [
+  "https://google.com",
+  "https://twitter.com",
+  "https://www.youtube.com",
+  "https://instagram.com",
+];
+
+site1Element.children[0].textContent = "Site 1";
+site1Element.children[1].setAttribute("href", "https://google.com");
+site1Element.children[1].children[0].setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1518780664697-55e3ad937233?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+);
+site1Element.children[1].children[0].setAttribute("alt", "image of your house");
+site1Element.children[1].children[0].setAttribute("style", "padding: 10px;");
+
+site2Element.children[0].textContent = "Site 2";
+site2Element.children[1].setAttribute("href", "https://twitter.com");
+site2Element.children[1].children[0].setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1599427303058-f04cbcf4756f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+);
+site2Element.children[1].children[0].setAttribute(
+  "alt",
+  "image of your momma house"
+);
+site2Element.children[1].children[0].setAttribute("style", "padding: 10px;");
+
+site3Element.children[0].textContent = "Site 3";
+site3Element.children[1].setAttribute("href", "https://instagram.com");
+site3Element.children[1].children[0].setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1625602812206-5ec545ca1231?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+);
+site3Element.children[1].children[0].setAttribute(
+  "alt",
+  "image of your dog house"
+);
+site3Element.children[1].children[0].setAttribute("style", "padding: 10px;");
+
+for (var i = 0; i < siteTitles.length; i++) {
+  siteTitles[i].setAttribute(
+    "style",
+    "text-decoration: underline; padding-left: 10px; margin: 0;"
+  );
+}
